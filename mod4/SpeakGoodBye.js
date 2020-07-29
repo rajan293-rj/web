0b21aaaa5e001fff1c99e04985c0f4d9f5ce0d91
rajan293-rj <60041013+rajan293-rj@users.noreply.github.com>
2020-07-29 11:47:25 +0530

    Add files via upload

---
+++ mod4/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+
+
+(function (window){
+	var byeSpeaker={};
+byeSpeaker.speakWord = "Good Bye";
+
+
+byeSpeaker.speak= function(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+window.byeSpeaker=byeSpeaker;
+
+}
+)(window);
+
